795be227ead4c2f745136bf068351b611e3df79b
fohlle <patte2000@hotmail.com>
2020-08-15 19:47:35 +0200

    new try

--- scripts/script.js
+++ scripts/script.js
@@ -28,23 +28,112 @@ function number(){
 
 }
 
-const btn = [...document.querySelectorAll("#container button")];
+const btnNum = [...document.querySelectorAll(".num")];
+const btnOp = [...document.querySelectorAll(".operator")];
+const btnClear = document.querySelector(".clear");
+const btnEqual = document.querySelector(".equal");
+
 const display = document.querySelector("#display");
 const para = document.createElement("p");
 
-btn.forEach(butts => {
-    butts.addEventListener("click" , event => {
-        let data = [...event.target.textContent];
-        if(event.target.textContent === "clear"){
-            data = [];
-            para.textContent = data;
+let data = "";
+let numbers = "";
+let operator = "";
+let array = [];
+let arrayTwo = [];
+let sum = 0;
+let text = "";
+
+
+btnNum.forEach(n => {
+    n.addEventListener("click", (e) => {
+        let hi = e.target.textContent;
+        array.push(hi);
+        if(sum === 0){
+            text = arrayTwo + " " + operator + " " + array.join("");
         }else{
-        para.textContent += data + " ";
+            text = sum + " " + operator + " " + array.join("");
         }
-        display.appendChild(para);
-       
-       console.log(event.target.textContent);
-       console.log(data)
+        appendPara(text);
     });
 });
-console.log(btn);
+
+btnOp.forEach(n => {
+    n.addEventListener("click", (e) => {
+        operator = e.target.textContent;
+        text += operator;
+        appendPara(text);
+        arrayTwo = array.join("");
+        array = [];
+        
+    })
+})
+
+btnClear.addEventListener("click", () => {
+    clear();
+    appendPara(array);
+});
+
+btnEqual.addEventListener("click", () => {
+    if(sum === 0){
+        sum = operate(operator,+array,+arrayTwo);
+    }else{
+        sum = operate(operator,sum,+array)
+
+    }
+    text = sum;
+    appendPara(text);
+})
+
+/*
+// numbers //
+btnNum.forEach(butts => {
+    butts.addEventListener("click" , event => {
+        data += event.target.textContent;
+        let text = numbers + " " + operator + " " + data;
+        appendPara(text)
+    });
+});
+// clearing //
+btnClear.addEventListener("click", () => {
+    clear();
+    sum = 0;
+    appendPara(data);
+})
+// operators //
+btnOp.forEach(butts => {
+    butts.addEventListener("click", (event) => {
+        operator = event.target.textContent;
+        if(sum === 0){
+            display.appendChild(para).textContent += operator;
+        }else{
+            display.appendChild(para).textContent = sum + " " + operator;
+        }
+        array.push(data);
+        numbers = data;
+        data = "";
+    });
+});
+// equals//
+btnEqual.addEventListener("click", () => {
+    if(sum === 0){
+        sum = operate(operator,+numbers,+data);
+    }else{
+        sum = operate(operator,sum,+data);
+    }
+    display.appendChild(para).textContent = sum;
+    clear();
+});
+*/
+function appendPara(string){
+    display.appendChild(para).textContent = string;
+}
+function clear(){
+    data = "";
+    numbers = "";
+    array = [];
+    operator = "";
+    arrayTwo = [];
+    sum = 0;
+    text = "";
+}
